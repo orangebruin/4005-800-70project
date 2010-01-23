8e98851f6206c684865eddb1b9924e221792e098
kcheek@gmail.com <kcheek@gmail.com@de5363ba-ff0c-11de-bce7-934f6fdf4811>
2010-01-23 21:27:57 +0000

    Initial revision

---
+++ run.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Arguments
+ALG=$1
+INPUT=$2
+
+# Settings
+RUNS="1 2 3"
+
+#OUTPUT=output.txt
+INPUT_DIR=Inputs/
+echo $ALG < $INPUT_DIR/input'$input'-1.txt  < INPUT_DIR/input$input-1.txt < INPUT_DIR/input$input-1.txt 
+
