615c4012c35a22a99d230f1878b8cda3b39fe95b
kcheek@gmail.com <kcheek@gmail.com@de5363ba-ff0c-11de-bce7-934f6fdf4811>
2010-01-25 07:50:58 +0000

    Modified to run with xcode enviroment

--- run.bash
+++ run.bash
@@ -3,11 +3,25 @@
 # Arguments
 ALG=$1
 INPUT=$2
+MODE=$3
 
-# Settings
-RUNS="1 2 3"
+# Set the path
+LCS_PATH=build/Debug/
 
-#OUTPUT=output.txt
+# Set LCS Name
+LCS=4005-800-70project
+
+#Set input directory location
 INPUT_DIR=Inputs/
-echo $ALG < $INPUT_DIR/input'$input'-1.txt  < INPUT_DIR/input$input-1.txt < INPUT_DIR/input$input-1.txt 
+
+# Get args from input file
+ARGS=( )
+let count=0
+while read line
+do
+	ARGS[$count]=$line
+	((count++))
+done < 'Inputs/Input'$INPUT'.txt'
+
+$LCS_PATH$LCS $ALG  ${ARGS[0]} ${ARGS[1]}
 
