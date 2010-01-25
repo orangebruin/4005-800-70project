76caadc67d715d29dd3af845c5cbf946a7601d92
kcheek@gmail.com <kcheek@gmail.com@de5363ba-ff0c-11de-bce7-934f6fdf4811>
2010-01-25 07:51:12 +0000

    Initial Revision

---
+++ trunk/timing.h
@@ -0,0 +1,8 @@
+#include <sys/time.h>
+#include <sys/resource.h>
+#define DEBUG
+extern int getrusage();
+#define CPUDEFS struct rusage ruse;
+#define CPUTIME (getrusage(RUSAGE_SELF,&ruse),\
+ruse.ru_utime.tv_sec + ruse.ru_stime.tv_sec + \
+1e-6 * (ruse.ru_utime.tv_usec + ruse.ru_stime.tv_usec))
