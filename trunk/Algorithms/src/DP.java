0eb8c1e58b8ad339f16fe6c909525d0eaf318c56
alb8951 <alb8951@de5363ba-ff0c-11de-bce7-934f6fdf4811>
2010-02-14 22:14:09 +0000

    Initial commit

---
+++ trunk/Algorithms/src/DP.java
@@ -0,0 +1,71 @@
+
+public class DP extends LCS {
+
+	@Override
+	public String Run(String S, String T) {
+		int cur, len1 = S.length(), len2 = T.length();
+		int mat[][] = new int[len1+1][len2+1];
+
+		//Start the timer
+		StartClock();
+
+		//Initialize table
+		for(int i = 0; i < len1+1; i++ ){
+			mat[i][0] = 0;
+		}
+
+		for(int j = 0; j < len2+1; j++){
+			mat[0][j] = 0;
+		}
+
+		//Fill in table
+		for( int i = 1; i < len1+1; i++ ){
+			for( int j = 1; j < len2+1; j++ ){
+				if( S.charAt(i-1) == T.charAt(j-1) ){
+					mat[i][j] = mat[i-1][j-1] + 1;
+				} else {
+					if( mat[i][j-1] > mat[i-1][j] )
+						mat[i][j] = mat[i][j-1];
+					else
+						mat[i][j] = mat[i-1][j];
+				}
+			}
+		}
+
+
+		//Find LCS
+		cur = mat[len1][len2];
+		char lcs[] = new char[cur];
+		cur--;
+		int x = len1;
+		int y = len2;
+		while( mat[x][y] != 0 ){
+			if((mat[x-1][y] == mat[x][y]-1) &&
+			   (mat[x][y-1] == mat[x][y]-1)){
+				lcs[cur] = S.charAt(x-1);
+				cur--;
+				x--;
+				y--;
+			} else if( mat[x-1][y] >= mat[x][y-1] ){
+				x--;
+			} else {
+				y--;
+			}
+		}
+
+		//Print table
+//		for( int i = 0; i < len1+1; i++ ){
+//			for( int j = 0; j < len2+1; j++){
+//				System.out.printf("%d ", mat[i][j]);
+//			}
+//			System.out.printf("\n");
+//		}
+//		System.out.println(lcs);
+
+		//Stop the timer
+		StopClock();
+
+		return new String(lcs);
+	}
+
+}
