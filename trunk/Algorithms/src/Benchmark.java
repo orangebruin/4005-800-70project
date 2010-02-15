6e6fd83c69c77b9ea733b075472fe8a4b27ed87f
alb8951 <alb8951@de5363ba-ff0c-11de-bce7-934f6fdf4811>
2010-02-15 03:45:36 +0000

    Added comments

--- trunk/Algorithms/src/Benchmark.java
+++ trunk/Algorithms/src/Benchmark.java
@@ -1,7 +1,17 @@
+/**
+ * Benchmark.java
+ *
+ * This class serves as the main class for running each of the LCS algorithms.
+ *
+ * @author Alex Brooks
+ * @author Kevin Cheek
+ */
+
 import java.util.Random;
 
 public class Benchmark {
 
+	//Properties governing the execution of the program
 	private static final int NUM_RUNS = Integer.parseInt(System.getProperty("nruns", "10") );
 	private static final long SEED = Long.parseLong(System.getProperty("seed", "0") );
 	private static final long RUNNING_TIME = Long.parseLong(System.getProperty("running_time", "10000"));
@@ -9,12 +19,17 @@ public class Benchmark {
 	private static final long INCREMENT_THRESHOLD = Long.parseLong(System.getProperty("it", "250") );
 	private static final int INCREMENT_AMOUNT = Integer.parseInt(System.getProperty("ia", "10") );
 
-
+	/**
+	 * The main method.
+	 *
+	 * @param args - the command line agruments
+	 */
 	public static void main(String[] args) {
 		LCS alg = null;
 
+		//Check if number of arguments is valid
 		if( args.length < 2 || args.length > 3){
-			System.err.println("Usage: LCS algorithm string1 string2 or \n");
+			System.err.println("Usage: LCS algorithm string1 string2 or ");
 			System.err.println("Usage: LCS algorithm character_set");
 			System.exit(1);
 		}
@@ -33,13 +48,13 @@ public class Benchmark {
 				break;
 			case 4:
 				alg = new Hirschberg();
-
 				break;
 			default:
 				//Invalid state
 				System.err.println("ERROR: Invalid Algorithm\n");
 		}
 
+		//Choose selected experiment
 		switch( args.length ){
 			case 3:
 				timing( alg, args[1], args[2]);
@@ -50,10 +65,20 @@ public class Benchmark {
 
 	}
 
+	/**
+	 * Determine the amount of time needed to find the LCS of two given strings
+	 * using the given algorithm. It is found by using the average time over a
+	 * number of runs. 10 runs is the default.
+	 *
+	 * @param alg  - The LCS algorithm to be used
+	 * @param str1 - The first input string
+	 * @param str2 - The second input string
+	 */
 	private static void timing( LCS alg, String str1, String str2 ){
-		long time=0;
-		int recursiveCalls=0;
-		String out;
+		long time=0;			//The total time taken
+		int recursiveCalls=0;	//The number of recursive calls
+		String out;				//The longest common subsequence
+
 		for( int i=0; i < NUM_RUNS; i++){
 			out = alg.Run(str1, str2);
 			if( VERBOSE ){
@@ -67,13 +92,23 @@ public class Benchmark {
 				" Total Time: " + time + " Recursive Calls (Last Run): " + recursiveCalls + "\n");
 	}
 
+	/**
+	 * Finds the length of the longest inputs that can be processed in a
+	 * certain amount of time by the given algorithm.
+	 * The default time is 10 seconds.
+	 *
+	 * @param alg			- The LCS algorithm
+	 * @param characterSet	- The alphabet to be used in creating the
+	 * 						  input strings
+	 */
 	private static void benchmark(LCS alg, String characterSet){
-		long totalTime = 0;
-		long time=0;
-		long prevTime=0;
-		int size=10;
-		int increment=10;
-		String out;
+		long totalTime = 0;		//The total time taken for test
+		long time = 0;			//Time for individual run
+		long prevTime = 0;		//Time for last run
+		int size = 10;			//The length of the inputs
+		int increment = 10;		//Value length is increased by
+		String out;				//The longest common subsequence
+
 		try{
 			while( time < RUNNING_TIME ){
 				prevTime = time;
@@ -91,11 +126,20 @@ public class Benchmark {
 				size += increment;
 			}
 		}catch(Exception e){
-			e.printStackTrace();
+			e.getMessage();
+		} finally {
+			System.out.println("Max String Size (2x): " + size + " Total Time: " + totalTime + " Last Run Time: "+ time);
 		}
-		System.out.println("Max String Size (2x): " + size + " Total Time: " + totalTime + " Last Run Time: "+ time);
 	}
 
+	/**
+	 * Generates a random string for use in the benchmark method.
+	 *
+	 * @param characterSet	- The alphabet to be used in creating
+	 * 						  the input string
+	 * @param length		- the desired length of the string
+	 * @return	the randomly created string
+	 */
 	private static String randomString( String characterSet, int length){
 		Random rand = new Random(SEED);
 		String out = "";
--- trunk/Algorithms/src/LCS.java
+++ trunk/Algorithms/src/LCS.java
@@ -1,11 +1,35 @@
-
+/**
+ * LCS.java
+ *
+ * This is the base class for each of the LCS algorithms.
+ *
+ * @author Alex Brooks
+ * @author Kevin Cheek
+ */
 public abstract class LCS {
+
+	//Start time for the algorithm run
 	protected long startTime=0;
+
+	//End time for the algorithm run
 	protected long endTime=0;
+
+	//The number of recursive calls done by the algorithm
 	protected int recursiveCalls=0;
 
+	/**
+	 * This method performs the algorithm.
+	 * It will be overridden by any subclass.
+	 *
+	 * @param S - The first input string
+	 * @param T - The second input string
+	 * @return	- The longest common subsequence of the two strings
+	 */
 	public abstract String Run( String S, String T );
 
+	/**
+	 * Starts the timer.
+	 */
 	protected void StartClock(){
 		if( startTime != 0)
 			throw new IllegalStateException("Error: Start time already initialized");
@@ -14,22 +38,38 @@ public abstract class LCS {
 
 	}
 
+	/**
+	 * Stops the timer.
+	 */
 	protected void StopClock(){
 		if( endTime != 0)
 			throw new IllegalStateException("Error: Start time already initialized");
 		endTime = System.currentTimeMillis();
 	}
 
+	/**
+	 * Returns the number of recursive calls made by the algorithm
+	 *
+	 * @return the number of recursive calls
+	 */
 	public int GetRecursiveCalls(){
 		return recursiveCalls;
 	}
 
+	/**
+	 * Resets the counters for the next run.
+	 */
 	public void ClearRunningTime(){
 		startTime = 0;
 		endTime = 0;
 		recursiveCalls = 0;
 	}
 
+	/**
+	 * This method calculates and returns the running time of the algorithm.
+	 *
+	 * @return the running time of the algorithm
+	 */
 	public long GetRunningTime(){
 		if( startTime == 0 || endTime == 0 )
 			throw new IllegalStateException("Start Clock or end Clock must be used");
